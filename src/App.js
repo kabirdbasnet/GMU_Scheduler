6539c66f8054e9f512df6da7277f8b15d3f664c9
Kabir Basnet <kabirbasnet@Kabirs-Air.eduroam.gmu.edu>
2023-10-07 14:02:04 -0400

    added home page component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import BlockLine from './/PageComponents/HomePage';  // Import the BlockLine component
 
-const App = ({ title }) => <div>{title}</div>;
+const App = ({ title }) => (
+  <div className="app-container">
+    <BlockLine />      {/* Include the BlockLine component below the title */}
+  </div>
+);
 
 export default App;
---
+++ src/PageComponents/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="green-block"></div>
+      <div className="yellow-line"></div>
+      <div className="home-page-title">Home</div>
+    </div>
+  );
+}
+
+export default HomePage;
